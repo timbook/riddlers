7ae05ee1a2cddae2c827d363b40f175feefb3cc2
TimothyKBook <TimothyKBook@gmail.com>
2017-05-07 19:23:20 -0400

    Added Lucky Derby riddler (in R)

---
+++ 20170505-lucky-derby/lucky-derby.R
@@ -0,0 +1,21 @@
+source('derby-functions.R')
+
+probs <- seq(0.52, 0.90, by = 0.02)
+
+runDerby <- function(probs) {
+  pos <- rep(0, 20)
+  
+  while (all(pos < 200)) {
+    movs <- 2 * rbinom(20, 1, probs)
+    pos <- pos + movs
+    pos <- ifelse(pos >= 0, pos, 0)
+  }
+  
+  return(which(pos >= 200))
+}
+
+nraces <- 1e5
+
+winners <- unlist(sapply(1:nraces, function(i) runDerby(probs)))
+
+prop.table(table(winners)) * 100
