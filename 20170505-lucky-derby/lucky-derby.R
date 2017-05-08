270e80b297a848def1683bd7d73a51cd59fd11c5
tim.book <tim.book@summitllc.us>
2017-05-08 09:14:46 -0400

    Fixed movement variable definition.

--- 20170505-lucky-derby/lucky-derby.R
+++ 20170505-lucky-derby/lucky-derby.R
@@ -1,12 +1,10 @@
-source('derby-functions.R')
-
 probs <- seq(0.52, 0.90, by = 0.02)
 
 runDerby <- function(probs) {
   pos <- rep(0, 20)
   
   while (all(pos < 200)) {
-    movs <- 2 * rbinom(20, 1, probs)
+    movs <- 2 * rbinom(20, 1, probs) - 1
     pos <- pos + movs
     pos <- ifelse(pos >= 0, pos, 0)
   }
