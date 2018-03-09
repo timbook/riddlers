a9ad5d2fde87b55c73e4509abbf4d5cae5a9be1c
Tim Book <tim.book@summitllc.us>
2018-03-09 13:47:51 -0500

    fixed express logic

--- 20180309-yahtzee/yahtzee-express.py
+++ 20180309-yahtzee/yahtzee-express.py
@@ -1,24 +1,22 @@
-from random import randint
 import numpy as np
 
 # Strategy: Reroll 5s.
 def scoreRoll(dice):
-    if all(d == 5 for d in dice):
+    if np.all(dice == 4):
         return 50
     else:
         return 12 + sum(dice)
 
 def playGame():
-    dice = [randint(1, 6), randint(1, 6)]
+    dice = np.random.randint(1, 7, 2)
     return scoreRoll(dice)
 
 ITER = int(1e5)
-scores = [0] * ITER
+scores = np.zeros(ITER)
 for i in range(ITER):
     score = playGame()
     scores[i] = score
 
-scores = np.array(scores)
 print("")
 print("Mean Score:")
 print(np.mean(scores))
